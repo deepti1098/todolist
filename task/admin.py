455ea8bf0668e5630f609d8adaf42088582fce6c
deepti1098 <66009385+deepti1098@users.noreply.github.com>
2022-02-15 21:50:10 +0530

    initial changes

---
+++ task/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import todolistItems
+
+admin.site.register(todolistItems)
---
+++ task/models.py
@@ -0,0 +1,4 @@
+from django.db import models
+
+class todolistItems(models.Model):
+    content = models.TextField()
