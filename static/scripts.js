c353a4c545377ffcae70d8cc3eb22336d038695c
abhishek-codes <43075875+abhishek-codes@users.noreply.github.com>
2022-02-19 23:10:06 +0530

    signup validation

--- static/scripts.js
+++ static/scripts.js
@@ -10,10 +10,10 @@ function matchPassword() {
   var pw1 = document.getElementById("pass1");
   var pw2 = document.getElementById("cpass2");
   if (pw1.value != pw2.value) {
-    alert("Passwords did not match");
+    $("#signupcpasserr").removeClass("removeele");
     return false;
   } else {
-    alert("Password created successfully");
+    $("#signupcpasserr").addClass("removeele");
     return true;
   }
 }  
