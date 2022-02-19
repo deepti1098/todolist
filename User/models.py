4b88ed5c15ab5098cf3c4d7ffa02f40d865f918e
abhishek-codes <43075875+abhishek-codes@users.noreply.github.com>
2022-02-19 14:22:11 +0530

    User Model Updated

--- User/models.py
+++ User/models.py
@@ -1,7 +1,10 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
 class User_detail(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True,default=1)
     name = models.CharField(max_length=40)
-    emailid = models.EmailField(primary_key=True)
-    password = models.CharField(max_length=40)
+    emailid = models.EmailField()
+    # password = models.CharField(max_length=40)
 
--- task/views.py
+++ task/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import ToDO
+from User.models import User_detail
 
 
 def todoView(request):
-    return render(request, 'list.html')
+    if not request.user.is_authenticated:
+        return render(request, 'login.html')
+    user = User_detail.objects.get(user=request.user)
+    ls = ToDO.objects.filter(user=user)
+    context = { 'todolist' : ls}
+    return render(request, 'list.html',context)
 
 
 def addtaskview(request):
